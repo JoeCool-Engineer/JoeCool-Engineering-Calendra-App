ab6af967b6f9870eb4f0222a4f141d5eb62b16e4
JoeCool-Engineer <rodney.buller2@gmail.com>
2025-09-22 21:50:33 -0700

    Added Navigation Bar with login using Clerk

--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,21 @@ export const DAYS_OF_WEEK_IN_ORDER = [
     "saturday",
     "sunday",
 ] as const;
+
+export const PrivateNavLinks = [
+{
+    imgURL: './events.svg',
+    label: 'My Events',
+    route: '/events'
+},
+{
+    imgURL: './schedule.svg',
+    label: 'My Schedule',
+    route: '/schedule'
+},
+{
+    imgURL: './public.svg',
+    label: 'Public Profile',
+    route: '/book'
+}
+] as const;
